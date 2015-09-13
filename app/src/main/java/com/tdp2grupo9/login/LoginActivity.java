37c4116b30c0bada7aae5d2232702319b05ef6ea
tomasnfranco@gmail.com <iNferiores89bitbucket>
2015-09-13 02:17:14 -0300

    Creo la Login Activity, con funcionalidad parcial para loguearse mediante email y la de logueo mediante Facebook casi terminada.

---
+++ app/src/main/java/com/tdp2grupo9/login/LoginActivity.java
@@ -0,0 +1,299 @@
+package com.tdp2grupo9.login;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.tdp2grupo9.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Muestra las opciones de loguearse con Facebook, email o registrar una cuenta.
+ */
+public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
+
+    private UserLoginTask authenticationTask = null;
+
+    private AutoCompleteTextView emailTextView;
+    private EditText passwordEditText;
+    private LoginButton facebookSignInButton;
+    private View progressView;
+    private View loginFormView;
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
+
+        createEmailTextView();
+        createPasswordTextView();
+        createEmailSignInButton();
+        createFacebookSignInButton();
+
+        loginFormView = findViewById(R.id.login_form);
+        progressView = findViewById(R.id.login_progress);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void createEmailTextView() {
+        emailTextView = (AutoCompleteTextView) findViewById(R.id.email);
+        populateAutoComplete();
+    }
+
+    private void createPasswordTextView() {
+        passwordEditText = (EditText) findViewById(R.id.password);
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void createEmailSignInButton() {
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+    }
+
+    private void createFacebookSignInButton() {
+        facebookSignInButton = (LoginButton) findViewById(R.id.facebook_sign_in_button);
+        facebookSignInButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // TODO: conectarse con el servidor y pasarle la informacion pertinente
+                Toast.makeText(getApplicationContext(), "ID: " + loginResult.getAccessToken().getUserId() + "\n"
+                                + "Token: loginResult.getAccessToken().getToken()",
+                                Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancel() {}
+
+            @Override
+            public void onError(FacebookException e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void populateAutoComplete() {
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    public void attemptLogin() {
+        if (authenticationTask != null) {
+            return;
+        }
+        resetErrors();
+        boolean validPassword = checkForValidPassword();
+        boolean validEmail = checkForValidEmailAddress();
+
+        if (!validEmail) {
+            emailTextView.requestFocus();
+        } else if (!validPassword) {
+            passwordEditText.requestFocus();
+        } else {
+            showProgress(true);
+            authenticationTask = new UserLoginTask();
+            authenticationTask.execute((Void) null);
+        }
+    }
+
+    private void resetErrors() {
+        emailTextView.setError(null);
+        passwordEditText.setError(null);
+    }
+
+    private boolean checkForValidPassword() {
+        String password = passwordEditText.getText().toString();
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            passwordEditText.setError(getString(R.string.error_invalid_password));
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkForValidEmailAddress() {
+        String email = emailTextView.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            emailTextView.setError(getString(R.string.error_field_required));
+            return false;
+        } else if (!isEmailValid(email)) {
+            emailTextView.setError(getString(R.string.error_invalid_email));
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isEmailValid(String email) {
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public void showProgress(final boolean show) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            loginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            progressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this, Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+                ContactsContract.Contacts.Data.MIMETYPE + " = ?",
+                new String[]{ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE},
+                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        List<String> emails = new ArrayList<String>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            emails.add(cursor.getString(ProfileQuery.ADDRESS));
+            cursor.moveToNext();
+        }
+        addEmailsToAutoComplete(emails);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {}
+
+    private interface ProfileQuery {
+        String[] PROJECTION = {
+                ContactsContract.CommonDataKinds.Email.ADDRESS,
+                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
+    }
+
+
+    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(LoginActivity.this,
+                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+        emailTextView.setAdapter(adapter);
+    }
+
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String email;
+        private final String password;
+
+        UserLoginTask() {
+            password = passwordEditText.getText().toString();
+            email = emailTextView.getText().toString();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                // TODO: aca va el codigo para conectarse con el servidor
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            // TODO: validar si el mail corresponde a una cuenta existente y si la password es la correcta
+            if (email.equals("a@b")) {
+                return password.equals("c");
+            }
+
+            // TODO: aca va el codigo para registrar una nueva cuenta.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            authenticationTask = null;
+            showProgress(false);
+
+            if (success) {
+                finish();
+            } else {
+                passwordEditText.setError(getString(R.string.error_incorrect_password));
+                passwordEditText.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            authenticationTask = null;
+            showProgress(false);
+        }
+    }
+}
+
