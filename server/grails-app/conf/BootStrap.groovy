f206358f1bc4fc70552e3c29cbef90ef548e2445
unknown <concdiego@gmail.com>
2015-09-19 11:57:52 -0300

    Se configuro el atributo Castrado a Si, No o Desconocido, para obtenerlos entrar a /api/castrado en GET, con el token correspondiente

--- server/grails-app/conf/BootStrap.groovy
+++ server/grails-app/conf/BootStrap.groovy
@@ -1,9 +1,13 @@
 import server.Usuario
+import server.Castrado
 class BootStrap {
 
     def init = { servletContext ->
-		new Usuario(facebookId:1156897635,username:'diegomeller',telefono:'112345600',direccion:'Paseo Colon 850').save()
-		new Usuario(username:'email',email:'diego@meller.com',password:'test',telefono:'1145670891',direccion:'Alguna').save()
+		new Usuario(facebookId:1156897635,username:'diegomeller',telefono:'112345600',direccion:'Paseo Colon 850',token:'12345').save()
+		new Usuario(username:'email',email:'diego@meller.com',password:'test',telefono:'1145670891',direccion:'Alguna',token:'234567').save()
+        new Castrado(tipo:'Si').save()
+        new Castrado(tipo:'No').save()
+        new Castrado(tipo:'Desconocido').save()
     }
     def destroy = {
     }
