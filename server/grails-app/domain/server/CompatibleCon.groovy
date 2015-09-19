541f1313587a0ff6fcd23113880101adc24ee7a0
unknown <concdiego@gmail.com>
2015-09-19 12:19:58 -0300

    Se configuro el atributo CompatibleCon, para obtenerlos entrar a /api/compatibleCon con el token correspondiente

--- server/grails-app/domain/server/CompatibleCon.groovy
+++ server/grails-app/domain/server/CompatibleCon.groovy
@@ -1,7 +1,9 @@
 package server
+import grails.rest.*
 
+@Resource(uri='/api/compatibleCon',formats=['json', 'xml'])
 class CompatibleCon {
-
+    String compatibleCon
     static constraints = {
     }
 }
