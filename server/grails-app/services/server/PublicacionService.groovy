4f933e1c708b141b21d9f8fed20e474e04cd7479
unknown <concdiego@gmail.com>
2015-09-25 22:43:14 -0300

    Primera prueba de la busqueda #3

---
+++ server/grails-app/services/server/PublicacionService.groovy
@@ -0,0 +1,41 @@
+package server
+
+import grails.transaction.Transactional
+
+@Transactional
+class PublicacionService {
+
+    def buscar(def params, def usuario) {
+        def busqueda = Publicacion.findAll(){
+            if(params.castrado)
+                castrado.id == params.castrado
+            if(params.color)
+                color.id == params.color
+            if(params.compatibleCon)
+                compatibleCon.id == params.compatibleCon
+            if(params.edad)
+                edad.id == params.edad
+            if(params.energia)
+                energia.id == params.energia
+            if(params.especie)
+                especie.id == params.especie
+            if(params.papelesAlDia)
+                papelesAlDia.id == params.papelesAlDia
+            if(params.proteccion)
+                proteccion.id == params.proteccion
+            if(params.sexo)
+                sexo.id == params.sexo
+            if(params.tamanio)
+                tamanio.id == params.tamanio
+            if(params.vacunasAlDia)
+                vacunasAlDia.id == params.vacunasAlDia
+            publicador.id != usuario.id
+            activa == true
+        }
+        def listado = busqueda
+        listado.each {it.setDistancia(usuario.latitud,usuario.longitud)}
+        listado.each{println it.distancia}
+        listado = listado.sort{it.distancia} //.collect{[id: it.id,publicador: it.publicador.username,distancia: it.distancia,foto: it.fotos ? it.fotos[0].base64 : '']}
+        return listado
+    }
+}
---
+++ server/test/unit/server/PublicacionServiceSpec.groovy
@@ -0,0 +1,20 @@
+package server
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(PublicacionService)
+class PublicacionServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
