d31e247250a1e3d05c36ce7ff3804f65ea62a383
romicasal <casal.romina@gmail.com>
2015-09-27 12:12:15 -0300

    Clase Connection para dejar en un solo lugar la ip del server y armar la url.

---
+++ app/src/main/java/com/tdp2grupo9/utils/Connection.java
@@ -0,0 +1,14 @@
+package com.tdp2grupo9.utils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.HttpURLConnection;
+
+public abstract class Connection {
+
+    private static final String SERVERURL = "http://192.168.1.3:8080/api/"; //TODO: pasar a un .config o algo
+    //10.0.3.2 genymotion
+    public static HttpURLConnection getHttpUrlConnection(String controller_action) throws IOException {
+        return (HttpURLConnection) new URL(SERVERURL + controller_action).openConnection();
+    }
+}
