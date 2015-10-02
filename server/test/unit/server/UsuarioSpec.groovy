6642ee44e125d84a527f63cab48c3a730e4aaafa
unknown <concdiego@gmail.com>
2015-10-01 22:43:19 -0300

    Terminadas las pruebas basicas sobre el usuario

--- server/test/unit/server/UsuarioSpec.groovy
+++ server/test/unit/server/UsuarioSpec.groovy
@@ -15,14 +15,48 @@ class UsuarioSpec extends Specification {
     def cleanup() {
     }
 
+    void "No permite Grabar usuario sin telefono"(){
+        when:
+        def user = new Usuario(facebookId: 12345,direccion:'abc')
+        user.validate()
+        then:
+        user.hasErrors()
+    }
+
+    void "No permite Grabar usuario sin direccion"(){
+        when:
+        def user = new Usuario(facebookId: 12345,telefono:'44444')
+        user.validate()
+        then:
+        user.hasErrors()
+    }
+
+    void "Usuario con fbid, telefono y direccion es correcto"(){
+        when:
+        def user = new Usuario(facebookId: 12345,telefono:'44444',direccion:'abcdefg')
+        then:
+        user.validate()
+        !(user.hasErrors())
+    }
+
+    void "Guarda Usuario y lo levanta por facebookId"(){
+        when:
+        def user = new Usuario(facebookId: 12345,telefono:'44444',direccion:'abcdefg')
+        user.save()
+        def userBuscado = Usuario.findByFacebookId(12345)
+        then:
+        userBuscado != null
+        userBuscado.id == user.id
+    }
 
     void "Unico facebookId para registrar"() {
         when:
-            new Usuario(facebookId: 12345).save()
-            def user2 = new Usuario(facebookId: 12345)
+            new Usuario(facebookId: 12345,direccion: 'abc',telefono: '12345').save()
+            def user2 = new Usuario(facebookId: 12345,direccion:'bcd',telefono:'234567')
             user2.validate()
         then:
             user2.hasErrors()
+            user2.errors['facebookId'] != null
     }
 
     //Hacer test para que no guarde usuario sin telefono ni direccion
