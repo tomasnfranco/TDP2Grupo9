a7e06433dfa6f26d0c08438af4e20ec48bd6434b
unknown <concdiego@gmail.com>
2015-10-11 12:31:36 -0300

    Creados los metodos para Crear, Eliminar y Ver una Alerta, Creado el metodo MisAlertas, #108.8,#108.9,#108.10,#108.11,#108.12

---
+++ server/grails-app/domain/server/Alerta.groovy
@@ -0,0 +1,35 @@
+package server
+
+class Alerta {
+    Usuario usuario
+    Date fechaCreacion = new Date()
+    String nombre = ""
+    //Atributos de Busqueda
+    Castrado castrado
+    Color color
+    CompatibleCon compatibleCon
+    Edad edad
+    Energia energia
+    Especie especie
+    PapelesAlDia papelesAlDia
+    Proteccion proteccion
+    Raza raza
+    Sexo sexo
+    Tamanio tamanio
+    VacunasAlDia vacunasAlDia
+    double latitud = 0
+    double longitud = 0
+    double distancia = 0
+
+    static constraints = {
+        usuario nullable: false
+        castrado nullable: true
+        compatibleCon nullable: true
+        energia nullable: true
+        papelesAlDia nullable: true
+        proteccion nullable: true
+        sexo nullable: true
+        tamanio nullable: true
+        vacunasAlDia nullable: true
+    }
+}
