473045f644e85b1f5ab46477fdc87ea3fd7e0616
unknown <concdiego@gmail.com>
2015-10-16 22:49:03 -0300

    Solo las FOTOS se pueden mandar como JSON

--- server/grails-app/conf/ApiFilters.groovy
+++ server/grails-app/conf/ApiFilters.groovy
@@ -8,10 +8,16 @@ class ApiFilters {
 				if(controllerName != 'usuario' || (actionName != 'save' && actionName != 'login')) {
 					//El usuario debe estar logueado, debe tener un token valido
 					//println "Entro en: $controllerName accion $actionName"
+					println params
+					println request.getJSON()
+					if(params.token == null){
+						params.token = request.getJSON()["token"]
+					}
 					if(params.token == null || params.token == ''){
 						render status: UNAUTHORIZED
 						return false
 					}
+
 					def userLogueado = Usuario.findByToken(params.token)
 					if(userLogueado == null){
 						render status: UNAUTHORIZED
